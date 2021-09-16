4deb714859074cf0710e4ddd6d7b6cf127c831e7
nicolas_carlini <nicolascarlini1@gmail.com>
2021-09-16 01:03:29 -0300

    rate limit improve

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, Response
 from redis import Redis
 import os
 import json
@@ -7,19 +7,27 @@ import json
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 s = requests.Session()
-IP_LIMIT = os.environ.get('ip_limit')
+IP_LIMIT = int(os.environ.get('IP_LIMIT', 6000))
+IP_LIMIT_PER_PATH = int(os.environ.get('IP_LIMIT_PER_PATH', 2000))
+URL = os.environ.get('TARGET', 'https://api.mercadolibre.com')
 
 @app.route('/')
-def hello():
-    redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
+def base():
+    return Response(status=200)
 
-@app.route('/p/<path:path>')
+@app.route('/<path:path>')
 def proxy(path):
     redis_ip_limit = redis.incr(request.remote_addr)
     redis_path_per_ip = redis.incr(f'{request.remote_addr}-{path}')
-    request
+    if redis_ip_limit <= IP_LIMIT and redis_path_per_ip <= IP_LIMIT_PER_PATH:
+        response = requests.request(request.method, url=URL+'/'+path, headers=request.headers,params=request.form,  data=request.get_json(), allow_redirects=True)
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in response.raw.headers.items() if name.lower() not in excluded_headers]
+        return Response(response.content, response.status_code, headers)
+
     return {'path':path,'method':request.method, 'headers': str(request.headers), 'request': request.method, 'ip': request.remote_addr, 'redis_ip_limit':redis_ip_limit, 'redis_path_per_ip':redis_path_per_ip}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
